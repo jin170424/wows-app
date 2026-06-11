985d2b4307a459525a5a5dd05a4cf8166cec01ef
Ooshima Zin <cyber@S-JoBi24-053.localdomain>
2026-06-11 11:00:10 +0900

    chore: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite' // ★1. これを追加！
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/wows-app/',
   plugins: [
     react(),
     tailwindcss() // ★2. これを追加！
