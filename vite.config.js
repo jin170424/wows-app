93799a1d8f67297557554c4a70d0ef6de8652c7b
Ooshima Zin <cyber@S-JoBi24-053.localdomain>
2026-06-18 09:26:10 +0900

    ci: dynamic base path for both Pages and Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // ★1. これを追加！
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wows-app/',
+  // ★GitHub Actions環境ならPages用のパス、Vercelやローカルならルートパス（/）に自動で切り替える魔法
+  base: process.env.GITHUB_ACTIONS ? '/wows-app/' : '/',
   plugins: [
     react(),
-    tailwindcss() // ★2. これを追加！
+    tailwindcss()
   ],
 })
